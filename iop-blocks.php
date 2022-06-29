efed8eecc655564b90b0f1ede5fcadac40deb970
Joe Maller <joe@joemaller.com>
2022-06-28 22:05:17 -0400

    1.0.1

--- iop-blocks.php
+++ iop-blocks.php
@@ -5,7 +5,7 @@
  * Description:       Plugin shell for our example blocks
  * Requires at least: 6.0
  * Requires PHP:      7.0
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Ideas On Purpose
  * License:           ISC
  * License URI:       https://opensource.org/licenses/isc-license.txt
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors:      Ideas On Purpose
 Tags:              block
 Tested up to:      5.9
-Stable tag:        1.0.0
+Stable tag:        1.0.1
 License:           MIT
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 
